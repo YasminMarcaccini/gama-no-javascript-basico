1572a962f08b957d23e01223e237b67808ba7761
Yasmin <yasminmarcacciniguide@gmail.com>
2022-05-11 20:12:28 -0300

    Montando o esqueleto das funções de validação do CPF

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
 console.log("Iniciando meu JavaScript");
 
+function validaCPF(){
+    if(){
+        //inserir validação do tamanho do cpf
+        //inserir ação caso o cpf não tenha 11 dígitos
+    }
+    else{
+        
+    }
+}
+
 function validacao(){
     console.log("Iniciando a validação do CPF");
+    if(validaCPF){
+        //inserir ação de mostrar que o CPF é válido
+    }
+    else{
+        //inserir ação de mostrar o display de CPF inválido
+    }
 };
