1183d8c032b02a8b6a04d8eebdbf731f0cb00802
Yasmin <yasminmarcacciniguide@gmail.com>
2022-05-11 20:44:11 -0300

    Adicionando validação de tamanho do CPF e mensagens de êxito e erro

--- script.js
+++ script.js
@@ -1,21 +1,29 @@
 console.log("Iniciando meu JavaScript");
 
-function validaCPF(){
-    if(){
-        //inserir validação do tamanho do cpf
+function validaCPF(cpf){
+    console.log(cpf.length);
+    //inserir validação do tamanho do cpf
+    if(cpf.length != 11){
         //inserir ação caso o cpf não tenha 11 dígitos
+        return false;
     }
     else{
-        
-    }
+        return true;
+    };
 }
 
 function validacao(){
     console.log("Iniciando a validação do CPF");
-    if(validaCPF){
+    var cpf = document.getElementById("cpf_digitado").value;
+
+    var resultadoValidacao = validaCPF(cpf);
+
+    if(resultadoValidacao){
         //inserir ação de mostrar que o CPF é válido
+        document.getElementById("success").style.display = "block";
     }
     else{
         //inserir ação de mostrar o display de CPF inválido
+        document.getElementById("error").style.display = "block";
     }
 };
